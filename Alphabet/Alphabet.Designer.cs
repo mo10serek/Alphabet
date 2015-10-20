2ce20aaf8b7edb7d5f8bb8fffdd86014a42a7fd2
Lakeadded <michael.balcerzak@ocsbstudent.ca>
2015-10-20 14:25:19 -0400

    This program show the alphabet

---
+++ Alphabet/Alphabet.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Alphabet
+{
+    partial class frmAlphabet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstAlphabet = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(68, 34);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstAlphabet
+            // 
+            this.lstAlphabet.FormattingEnabled = true;
+            this.lstAlphabet.Location = new System.Drawing.Point(48, 87);
+            this.lstAlphabet.Name = "lstAlphabet";
+            this.lstAlphabet.Size = new System.Drawing.Size(120, 95);
+            this.lstAlphabet.TabIndex = 1;
+            // 
+            // frmAlphabet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 221);
+            this.Controls.Add(this.lstAlphabet);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmAlphabet";
+            this.Text = "Alphabet";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstAlphabet;
+    }
+}
+
